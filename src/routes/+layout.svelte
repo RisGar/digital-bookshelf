242c0c655b2c592330b7c8f82b3f31972c835ab4
Rishab Garg <rishgarg@gmail.com>
2023-02-05 19:20:39 +0100

    feat!: initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import '../app.css'
+</script>
+
+<div class="bg-neutral text-neutral-content">
+	<div class="max-w-xl mx-auto navbar">
+		<div class="navbar-start">
+			<a href="/" class="btn btn-ghost text-xl">digital-bookshelf</a>
+		</div>
+	</div>
+</div>
+
+<div class="max-w-xl mx-auto py-8 px-4">
+	<slot />
+</div>
+
+<footer>
+	<p>&copy; {new Date().getFullYear()} Rishab Garg</p>
+</footer>
